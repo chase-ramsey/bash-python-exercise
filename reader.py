b472fa942edcc7b0264d1c19fd52b439f6247756
Chase Ramsey <ramsey.chase@gmail.com>
2016-07-29 12:43:36 -0500

    Completed reader module

---
+++ reader.py
@@ -0,0 +1,14 @@
+import sys
+import re
+
+class Reader():
+
+  def __init__(self):
+    self.file = open(sys.argv[1] + '.txt')
+    self.items = list()
+
+  def concat_items(self):
+    [self.items.append(item[:-1]) for item in self.file]
+    self.items = [re.sub(r'\s', '_', item) for item in self.items]
+    items_string = (' ').join(self.items)
+    return items_string
