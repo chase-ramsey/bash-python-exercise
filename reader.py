549f2d6820ec6867572b0db8ecf4a3825545c115
Chase Ramsey <ramsey.chase@gmail.com>
2016-07-29 14:40:39 -0500

    Got rid of self.file in Reader and used with block instead

--- reader.py
+++ reader.py
@@ -4,11 +4,12 @@ import re
 class Reader():
 
   def __init__(self):
-    self.file = open(sys.argv[1] + '.txt')
     self.items = list()
 
   def concat_items(self):
-    [self.items.append(item[:-1]) for item in self.file]
+    with open(sys.argv[1] + '.txt') as file:
+      [self.items.append(item[:-1]) for item in file]
+
     self.items = [re.sub(r'\s', '_', item) for item in self.items]
     items_string = (' ').join(self.items)
     return items_string
